1e5843b9a673b9848963b92ff824f7fa82ace446
sunpengfxq <50624970+sunpengfxq@users.noreply.github.com>
2019-05-14 22:31:52 +0800

    Update UserController.java

--- springcloud-consumer/src/main/java/com/jk/controller/UserController.java
+++ springcloud-consumer/src/main/java/com/jk/controller/UserController.java
@@ -27,6 +27,7 @@ public class UserController {
     public List<User> queryGoodsList(){
 
         System.out.println(111);
+        System.out.println(222);
         return userService.queryGoodsList();
     }
 }
